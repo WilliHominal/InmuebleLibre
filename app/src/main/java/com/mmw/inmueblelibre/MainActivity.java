876b2999dadc3a00bb67d6608625eb8510fdee43
WilliHominal <willi.hominal97@gmail.com>
2021-12-13 20:05:57 -0300

    Firebase DataBase + Authentication + pantalla Registrarse

--- app/src/main/java/com/mmw/inmueblelibre/MainActivity.java
+++ app/src/main/java/com/mmw/inmueblelibre/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button botonPruebaAgregarInmueble;
+    Button botonPruebaRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +18,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         botonPruebaAgregarInmueble = (Button) findViewById(R.id.prueba_agregar_inmueble_BTN);
+        botonPruebaRegistrarse = (Button) findViewById(R.id.prueba_registrarse_BTN);
 
         botonPruebaAgregarInmueble.setOnClickListener(this);
+        botonPruebaRegistrarse.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.prueba_agregar_inmueble_BTN:
-                Intent intent = new Intent(MainActivity.this, AgregarInmuebleActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this, AgregarInmuebleActivity.class));
+                break;
+            case R.id.prueba_registrarse_BTN:
+                startActivity(new Intent(MainActivity.this, RegistrarActivity.class));
         }
     }
 }
---
+++ app/src/main/java/com/mmw/inmueblelibre/RegistrarActivity.java
@@ -0,0 +1,99 @@
+package com.mmw.inmueblelibre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegistrarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText nombreET;
+    private EditText emailET;
+    private EditText contrasenaET;
+    private Button registrarseBTN;
+    private RadioGroup tipoUsuarioRG;
+
+    private String nombre;
+    private String email;
+    private String contrasena;
+    private String tipo;
+
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+
+        nombreET = (EditText) findViewById(R.id.REGISTRAR_nombre_ET);
+        emailET = (EditText) findViewById(R.id.REGISTRAR_email_ET);
+        contrasenaET = (EditText) findViewById(R.id.REGISTRAR_contrasena_ET);
+        registrarseBTN = (Button) findViewById(R.id.REGISTRAR_registrarse_BTN);
+        tipoUsuarioRG = (RadioGroup) findViewById(R.id.REGISTRAR_tipo_usuario_RG);
+
+        registrarseBTN.setOnClickListener(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.REGISTRAR_registrarse_BTN:
+                nombre = nombreET.getText().toString();
+                email = emailET.getText().toString();
+                contrasena = contrasenaET.getText().toString();
+                tipo = tipoUsuarioRG.getCheckedRadioButtonId() == R.id.REGISTRAR_propietario_RB ? "PROPIETARIO" : "CLIENTE";
+
+                if (nombre.isEmpty() || email.isEmpty() || contrasena.isEmpty()){
+                    Toast.makeText(RegistrarActivity.this, "Debe completar los campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (contrasena.length() < 6){
+                        Toast.makeText(RegistrarActivity.this, "La contraseña debe tener 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+                    registrarUsuario();
+                }
+                break;
+        }
+    }
+
+    private void registrarUsuario(){
+        firebaseAuth.createUserWithEmailAndPassword(email, contrasena).addOnCompleteListener(task -> {
+           if (task.isSuccessful()){
+               Map<String, Object> mapaValores = new HashMap<>();
+               mapaValores.put("nombre", nombre);
+               mapaValores.put("email", email);
+               mapaValores.put("contrasena", contrasena);
+               mapaValores.put("tipo", tipo);
+
+               String id = firebaseAuth.getCurrentUser().getUid();
+
+               databaseReference.child("Usuarios").child(id).setValue(mapaValores).addOnCompleteListener(taskDB -> {
+                   if (taskDB.isSuccessful()){
+                       startActivity(new Intent(RegistrarActivity.this, MainActivity.class));
+                       finish();
+                   } else {
+                       Toast.makeText(getApplicationContext(), "No se pudieron crear los datos correctamente", Toast.LENGTH_SHORT).show();
+                   }
+               });
+
+           } else {
+               Toast.makeText(getApplicationContext(), "No se pudo registrar el usuario", Toast.LENGTH_SHORT).show();
+           }
+        });
+    }
+}
