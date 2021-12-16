6add70c60c0968ff0210d3a6a9f6948d013f6870
WilliHominal <willi.hominal97@gmail.com>
2021-12-16 00:22:21 -0300

    +Vista Inmueble con mapa

--- app/src/main/java/com/mmw/inmueblelibre/adapter/InmuebleAdapter.java
+++ app/src/main/java/com/mmw/inmueblelibre/adapter/InmuebleAdapter.java
@@ -1,13 +1,25 @@
 package com.mmw.inmueblelibre.adapter;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mmw.inmueblelibre.R;
 import com.mmw.inmueblelibre.model.InmuebleModel;
 
@@ -25,9 +37,10 @@ public class InmuebleAdapter extends RecyclerView.Adapter<InmuebleAdapter.ViewHo
         InmuebleAdapter.listener = listener;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements OnMapReadyCallback {
         TextView idInput;
-        TextView direccionInput;
+        MapView minimapa;
+        GoogleMap map;
         TextView precioInput;
         View view;
 
@@ -35,8 +48,15 @@ public class InmuebleAdapter extends RecyclerView.Adapter<InmuebleAdapter.ViewHo
             super(vistaInmueble);
             view = vistaInmueble;
             idInput = vistaInmueble.findViewById(R.id.INM_id_TV);
-            direccionInput = vistaInmueble.findViewById(R.id.INM_direccion_TV);
+            minimapa = vistaInmueble.findViewById(R.id.INM_minimapa);
             precioInput = vistaInmueble.findViewById(R.id.INM_precio_TV);
+
+            if (minimapa != null) {
+                minimapa.onCreate(null);
+                minimapa.onResume();
+                minimapa.getMapAsync(this);
+            }
+
         }
 
         public View contenedor(){
@@ -47,14 +67,31 @@ public class InmuebleAdapter extends RecyclerView.Adapter<InmuebleAdapter.ViewHo
             return idInput;
         }
 
-        public TextView getDireccionInput(){
-            return direccionInput;
+        public MapView getDireccionInput(){
+            return minimapa;
         }
 
         public TextView getPrecioInput(){
             return precioInput;
         }
 
+        @Override
+        public void onMapReady(@NonNull GoogleMap googleMap) {
+            MapsInitializer.initialize(view.getContext());
+            map = googleMap;
+            map.getUiSettings().setAllGesturesEnabled(false);
+            setearUbicacion();
+        }
+
+        private void setearUbicacion(){
+            if (map == null) return;
+
+            LatLng ubi = (LatLng) minimapa.getTag();
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(ubi, 18));
+            map.addMarker(new MarkerOptions().position(ubi));
+
+            map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
     }
 
     public InmuebleAdapter(List<InmuebleModel> dataSet){
@@ -72,9 +109,15 @@ public class InmuebleAdapter extends RecyclerView.Adapter<InmuebleAdapter.ViewHo
 
         InmuebleModel inmuebleTemp = inmuebleDataSet.get(position);
 
+        String coordsString = inmuebleTemp.getDireccion().substring(1, inmuebleTemp.getDireccion().length()-1);
+        Double latTemp = Double.valueOf(coordsString.split(",")[0]);
+        Double lonTemp = Double.valueOf(coordsString.split(",")[1]);
+        LatLng coords = new LatLng(latTemp, lonTemp);
+
+        viewHolder.minimapa.setTag(coords);
+
         viewHolder.idInput.setText("#" + inmuebleTemp.getId());
-        viewHolder.direccionInput.setText(inmuebleTemp.getDireccion());
-        viewHolder.precioInput.setText(inmuebleTemp.getPrecio());
+        viewHolder.precioInput.setText("$" + inmuebleTemp.getPrecio());
     }
 
     @Override
